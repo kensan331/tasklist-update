cc07dc29a7064298bc277592a3464387988513da
ken.nmegane <tibikengame@gmail.com>
2019-09-04 14:54:15 +0000

    correction

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task =Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,25 +17,24 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:succes] = 'Taskが正常に追加されました'
-      redirect_to @task
+      flash[:success] = 'Taskが正常に追加されました'
+      redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Taskが追加されませんでした'
-      render :'tasks/index'
+      render 'new'
     end
   end
 
   def edit
-    @task =Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
     @task = Task.find(params[:id])
-    
     if @task.update(task_params)
-      flash[:succes] = 'Taskが正常に更新されました'
-      redirect_to @task
+      flash[:success] = 'Taskが正常に更新されました'
+       redirect_to root_url
     else
       flash.now[:danger] = 'Taskは更新されませんでした'
       render :edit
